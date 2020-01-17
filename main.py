17b83159d4213f62d6bd9fa9168475e63e3f7a22
zypherion <zxcbvnm90@gmail.com>
2020-01-17 20:56:00 +0000

    Fixing the rounding errors.

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ import math
 #Query user for decimal formatted hours worked:
 timeWorked = float(input("How many hours have you already worked: "))
 
+#clear screen:
+print(chr(27)+'[2j')
+print('\033c')
+print('\x1bc')
+wColor = '\033[32m'
+lColor = '\033[31m'
+dColor = '\033[m'
+
 #split decimal formatted hours worked into int holding hours and int holding minutes
 minutesWorked, hoursWorked = math.modf(timeWorked)
-minutesWorked = int(minutesWorked * 60)
+minutesWorked = math.ceil(minutesWorked * 60)
 
 #Convert all time worked into minutes, then subtract from full week:
 timeWorked = timeWorked * 60 
@@ -14,6 +22,7 @@ timeLeft = 2400 - timeWorked
 
 #divide timeleft by sixty to convert back into 
 minutesLeft, hoursLeft = math.modf((timeLeft / 60))
-minutesLeft = int(minutesLeft * 60 + 1)
-print ("You've worked %d hours and %d minutes so far."% (hoursWorked, minutesWorked))
-print ("You still need to work %d hours and %d minutes to reach 40hrs." % (hoursLeft, minutesLeft))
+minutesLeft = math.ceil(minutesLeft * 60)
+
+print ("You've worked %s%d hours and %d%s minutes so far."% (wColor, hoursWorked, minutesWorked, dColor))
+print ("You still need to work %s%d hours and %d%s minutes to reach 40hrs." % (lColor, hoursLeft, minutesLeft, dColor))
